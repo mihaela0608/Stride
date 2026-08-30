5a178c048f5c34cb8022ffdbf51205b85e15974b
mihaela0608 <mihaelabataklieva2006@gmail.com>
2026-08-30 13:04:50 +0300

    Initial commit

---
+++ src/main/java/org/example/stride/StrideApplication.java
@@ -0,0 +1,13 @@
+package org.example.stride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stride/StrideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
